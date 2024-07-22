9b3a8ff4d59f08e954b9e2015d49b46092f6e344
Munajat Ibnu <78370273+mywidget@users.noreply.github.com>
2024-07-22 20:19:44 +0700

    Create form_status_error.php

---
+++ application/views/frontend/form_status_error.php
@@ -0,0 +1,7 @@
+<div class="shadow rounded-12 p-3 mb-3">
+    <hr class="my-2">
+    <div class="d-flex justify-content-between">
+        <strong> Maaf data tidak ditemukan</strong>
+    </div>
+    
+</div>
