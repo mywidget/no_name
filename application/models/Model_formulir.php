b8e387d568645e32a5fcd853032149d602c8047a
Munajat Ibnu <78370273+mywidget@users.noreply.github.com>
2024-07-24 01:34:45 +0700

    Update Model_formulir.php

--- application/models/Model_formulir.php
+++ application/models/Model_formulir.php
@@ -30,6 +30,67 @@
 			return $result;
 		}	
 		
+		public function get_pesan_pendaftaran($post)
+		{
+			$this->db->select('deskripsi');
+			$this->db->from('rb_template_pesan');
+			$this->db->where('slug','PENDAFTARAN');
+			$this->db->where('aktif','Ya');
+			$this->db->limit(1);
+			$query = $this->db->get(); 
+			
+			if($query->num_rows() > 0){ 
+				$row = $query->row();
+				$biaya = convert_to_number($post['biaya']);
+				$searchVal = array(
+				"{selamat}",
+				"{nama_sekolah}",
+				"{web_sekolah} ",
+				"{wa_sekolah}",
+				"{email_sekolah}",
+				"{alamat_sekolah}",
+				"{nomor_pendaftaran}",
+				"{tgl_pendaftaran}",
+				"{nama_pendaftar}",
+				"{nik}",
+				"{nisn}",
+				"{email_pendaftar}",
+				"{unit}",
+				"{kelas}",
+				"{kamar}",
+				"{biaya}",
+				"{cetak_formulir}"
+				);
+				
+				$link = tag_key('site_url').'/cetak-formulir/'.encrypt_url($post['nik']);
+				// Array containing replace string from  search string
+				$replaceVal = array(
+				ucapan(),
+				info('nama_sekolah'),
+				info('site_url'),
+				info('whatsapp'),
+				info('site_mail'),
+				info('site_addr'),
+				$post['nik'],
+				date('Y-m-d'),
+				$post['nama'],
+				$post['nik'],
+				$post['nisn'],
+				$post['email'],
+				$post['unit_sekolah'],
+				$post['kelas'],
+				$post['kamar'],
+				rp($biaya),
+				$link
+				);
+				
+				// Function to replace string
+				$pesan = str_replace($searchVal, $replaceVal, $row->deskripsi);
+				
+				return $pesan;
+			}
+		}	
+		
 		public function get_pesan($post)
 		{
 			$this->db->select('deskripsi');
