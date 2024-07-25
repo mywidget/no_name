29616a2cd9c33336343d19288a0def6d2089d4b2
Munajat Ibnu <78370273+mywidget@users.noreply.github.com>
2024-07-25 09:21:17 +0700

    Update syarat.php

--- application/views/frontend/syarat.php
+++ application/views/frontend/syarat.php
@@ -1,7 +1,14 @@
 <main class="container my-4">
-    <div class="shadow rounded-12 p-3">
-        <h4 class="my-4 text-dark"><?=$row->title;?> <?php echo $tahun['nama_tahun'];?></h4>
-        <hr class="my-2">
-        <?=$row->deskripsi;?>
-    </div>
+    <?php if(!empty($row)){ ?>
+        <div class="shadow rounded-12 p-3">
+            <h4 class="my-4 text-dark"><?=$row->title;?> <?php echo $tahun['nama_tahun'];?></h4>
+            <hr class="my-2">
+            <?=$row->deskripsi;?>
+        </div>
+        <?php }else{ ?>
+        <div class="shadow rounded-12 p-3">
+            <h4 class="my-4 text-dark">Belum ada data</h4>
+            <hr class="my-2">
+        </div>
+    <?php } ?>
 </main>
