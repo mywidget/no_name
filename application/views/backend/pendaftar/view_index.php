1ee338d468bc504c9a3aa9aeb187dda01975675d
Munajat Ibnu <78370273+mywidget@users.noreply.github.com>
2024-07-28 00:40:57 +0700

    Update view_index.php

--- application/views/backend/pendaftar/view_index.php
+++ application/views/backend/pendaftar/view_index.php
@@ -193,7 +193,7 @@
 					
 				</div>
                 <div class="modal-footer">
-                    <button class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                   <button type="button" class="btn me-auto" data-bs-dismiss="modal">Close</button>
                     <button class="btn btn-primary">Submit</button>
 				</div>
 			</form>
