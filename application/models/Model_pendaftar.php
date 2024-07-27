e360a27d103268260b4f469fc3b8d8c12cb375cc
Munajat Ibnu <78370273+mywidget@users.noreply.github.com>
2024-07-27 13:28:35 +0700

    Update Model_pendaftar.php

--- application/models/Model_pendaftar.php
+++ application/models/Model_pendaftar.php
@@ -735,6 +735,7 @@
 			/* Query */
 			$this->db->select("*");
 			$this->db->where('tahun_akademik', $filter);
+			$this->db->where('s_pendidikan', 'Baru');
 			$this->db->order_by('nama', 'ASC'); 
 			$query = $this->db->get('rb_psb_daftar');
 			return $query->result_array();
