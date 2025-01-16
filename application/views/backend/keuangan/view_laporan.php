382b73825c6ab27b2a230fa361203f21d0085e56
Munajat Ibnu <78370273+mywidget@users.noreply.github.com>
2025-01-16 20:32:35 +0700

    Update view_laporan.php

--- application/views/backend/keuangan/view_laporan.php
+++ application/views/backend/keuangan/view_laporan.php
@@ -24,6 +24,7 @@
 						<h3 class="card-title d-print-none">List Data</h3>
 						<div class="col-auto ms-auto d-print-none d-none" id="btn-print">
 							<a href="/keuangan/export_to_excel" class="btn btn-success" target="_blank">
+							 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" /><path d="M8 11h8v7h-8z" /><path d="M8 15h8" /><path d="M11 11v7" /></svg>
 								Export Laporan
 							</a>
 							<button type="button" class="btn btn-primary" onclick="javascript:window.print();">
